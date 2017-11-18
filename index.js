1eaf41ff74102af0ab81e7759c6b80e62f1073ce
Onur Eker <yonureker@gmail.com>
2017-11-18 05:26:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function deleteFromDriverByKey(driver, key){
 }
 
 function destructivelyDeleteFromdriverByKey(driver, key){
-  delete driver.key;
-  return driver;
+  newDriver = delete driver.key;
+  return newDriver;
 }
